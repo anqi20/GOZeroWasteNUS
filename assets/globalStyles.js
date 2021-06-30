57b66b29b6958292f350b4a206e9b836560fd40c
anqi20 <anqi20@hotmail.com>
2021-06-30 20:01:19 +0800

    Created globalStyles

--- assets/globalStyles.js
+++ assets/globalStyles.js
@@ -2,32 +2,50 @@ import { StyleSheet } from 'react-native';
 import colors from "../assets/colors";
 
 export const globalStyles = StyleSheet.create({
-    button: {
+    //first button 
+    buttonTop: {
         height: 30,
         backgroundColor: colors.black,
-        marginHorizontal: 50,
+        marginTop: 90,
+        marginHorizontal: 30,
         borderRadius: 25,
         justifyContent: "center",
         alignItems: "center",
+    }, 
+    //subsequent buttons
+    button: {
+      height: 30,
+      backgroundColor: colors.black,
+      marginTop: 10, 
+      marginHorizontal: 30,
+      borderRadius: 25,
+      justifyContent: "center",
+      alignItems: "center",
     }, 
     buttonText: {
         fontSize: 14,
         color: colors.white,
         justifyContent: "center",
     },
+    //first input container
+    inputContainerTop: {
+      marginTop: 20,
+      paddingHorizontal: 30,
+    },
+    //subsequent input container
     inputContainer: {
-        marginTop: 20,
-        paddingHorizontal: 25,
-      },
-      inputLabel: {
-        color: colors.black,
-        marginBottom: -5,
-      },
-      inputInput: {
-        fontSize: 14,
-      },
-      inputError: {
-        alignSelf: "center",
-        color: colors.red,
-      },
+      marginTop: 10, 
+      paddingHorizontal: 30,
+    },
+    inputLabel: {
+      color: colors.black,
+      marginBottom: -5,
+    },
+    inputInput: {
+      fontSize: 14,
+    },
+    inputError: {
+      alignSelf: "center",
+      color: colors.red,
+    },
 });

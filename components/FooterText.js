8f1fa492104ad1e526476a1dbef23415e23c4383
tsh22 <sihuitoh22@gmail.com>
2021-07-10 00:50:21 +0800

    Implement navigation from footer text

--- components/FooterText.js
+++ components/FooterText.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import colors from "../assets/colors";
+import { useNavigation } from "@react-navigation/native";
 
 export default function FooterText() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text style={styles.subtitle}>
-        If there is any error, please feedback to us or call the helpdesk{"\n"}
+        If there is any error, please{" "}
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Feedback to us!")}
+        >
+          <Text style={styles.clickable}>feedback to us</Text>
+        </TouchableOpacity>{" "}
+        or call the helpdesk{"\n"}
         @1234 5678!
       </Text>
     </View>
@@ -21,5 +30,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: colors.darkGrey,
     textAlign: "center",
+  },
+  clickable: {
+    fontSize: 18,
+    color: colors.darkGrey,
+    textDecorationLine: "underline",
   },
 });

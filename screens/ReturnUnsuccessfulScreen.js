0e35497b06a4b0f15b01d433126b1e3528d511e8
anqi20 <anqi20@hotmail.com>
2021-07-16 01:20:48 +0800

    Created return error page

---
+++ screens/ReturnUnsuccessfulScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, ScrollView } from "react-native";
+import UnsuccessBox from "../components/UnsuccessBox";
+import colors from "../assets/colors";
+import { globalStyles } from "../assets/globalStyles";
+
+export default function ReturnUnsuccessfulScreen() {
+
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <Text style={globalStyles.header}>Return</Text>
+      <UnsuccessBox />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 40,
+  },
+});

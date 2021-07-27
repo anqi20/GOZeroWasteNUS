a2995f52b9fc8839e12335703e9e55a7529981f7
anqi20 <anqi20@hotmail.com>
2021-07-28 00:10:59 +0800

    Fixed issues with gender, DOB, faculty fields

--- screens/authentication-screens/SignUpDetailScreen.js
+++ screens/authentication-screens/SignUpDetailScreen.js
@@ -1,17 +1,74 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import * as yup from "yup";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView 
+} from "react-native";
 import { Formik } from "formik";
 import { Input } from "react-native-elements"; 
 import { globalStyles } from "../../assets/globalStyles";
-import { Ionicons, FontAwesome5, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { Ionicons, FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../../assets/colors";
+import moment from "moment";
+import RNPickerSelect from "react-native-picker-select";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Icon } from "react-native-elements";
 
 export default function SignUpDetailScreen({ navigation }) {
+  const [date, setDate] = useState(new Date());
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isDateConfirmed, setDateConfirmed] = useState(false);
+
+  //Date picker methods
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  }
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  }
+
+  const validationSchema = yup.object().shape({
+    firstName: yup.string()
+      .required('Please enter your first name'),
+    lastName: yup.string()
+      .required('Please enter your last name'),
+    gender: yup.string()
+      .required('Please choose your gender'),
+    dOB: yup.string()
+      .required('Please choose your date of birth'),
+    faculty: yup.string()
+      .required('Please choose your faculty'),
+    password: yup.string()
+      .label('Password')
+      .required('Please enter your new password')
+      .min(8, 'Password must have at least 8 characters')
+      .matches(/[0-9]/, 'Password must contain at least a digit')
+      .matches(/[A-Z]/, 'Password must contain at least a capital letter')
+      .matches(/[a-z]/, 'Password must contain at least a small letter')
+      .matches(/[^(0-9,A-Z,a-z)]/, 'Password must contain a special character'),
+    confirmPassword: yup.string()
+      .label('confirmPassword')
+      .required('Please confirm your new password')
+      .oneOf([yup.ref('password'), null], 'Both passwords do not match. Please check again.')
+  })
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView 
+      behavior="position"
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ScrollView showsVerticalScrollIndicator={false}>
       <Formik
         initialValues={{
-          username:"", 
+          email: "email", 
           firstName:"", 
           lastName:"", 
           gender: "", 
@@ -20,109 +77,176 @@ export default function SignUpDetailScreen({ navigation }) {
           password:"", 
           confirmPassword:"",
         }}
+        validationSchema = {validationSchema}
         onSubmit={(values) => {
-
+          navigation.navigate("Main Tab Navigator")
         }}
       >
 
-        {(props) => (
+        {({ handleChange, handleSubmit, setFieldValue, errors, isValid }) => (
           <View>
             <Input 
               containerStyle={globalStyles.inputContainerTop}
               placeholder="NUS email"
               inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <Ionicons name="mail" size={24}/>
-              }
-              onChangeText={props.handleChange("username")}
-              value={props.values.username}
+              leftIcon={<Ionicons name="mail" size={24}/>}
+              onChangeText={handleChange("email")}
+              autoCapitalize="none"
+              editable={false}
             />
             <Input 
               containerStyle={globalStyles.inputContainerNormal}
               placeholder="First Name (Family Name)"
               inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <FontAwesome5 name="user-alt" size={24}/>
-              }
-              onChangeText={props.handleChange("firstName")}
-              value={props.values.firstName}
+              leftIcon={<FontAwesome5 name="user-alt" size={24}/>}
+              onChangeText={handleChange("firstName")}
             />
+            <Text style={globalStyles.inputError}>{errors.firstName}</Text>
+
             <Input 
               containerStyle={globalStyles.inputContainerNormal}
               placeholder="Last Name"
               inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <FontAwesome5 name="user-alt" size={24}/>
-              }
-              onChangeText={props.handleChange("lastName")}
-              value={props.values.lastName}
+              leftIcon={<FontAwesome5 name="user-alt" size={24}/>}
+              onChangeText={handleChange("lastName")}
             />
-            <Input 
-              containerStyle={globalStyles.inputContainerNormal}
-              placeholder="Gender"
-              inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <MaterialCommunityIcons name="gender-male-female" size={24}/>
-              }
-              onChangeText={props.handleChange("gender")}
-              value={props.values.gender}
+            <Text style={globalStyles.inputError}>{errors.lastName}</Text>
+
+            <RNPickerSelect 
+              onValueChange={(value) => setFieldValue("gender", value)}
+              items={gender}
+              placeholder={{
+                label: "Gender", 
+                value: null,
+              }}
+              useNativeAndroidPickerStyle={false}
+              style={{
+                ...pickerSelectStyles, 
+                iconContainer:{ left: 30 }, 
+                placeholder: styles.dropdownPlaceholder,
+              }}
+              Icon={() => {
+                return <MaterialCommunityIcons name="gender-male-female" size={24}/>
+              }}
             />
-            <Input 
-              containerStyle={globalStyles.inputContainerNormal}
-              placeholder="Date of Birth"
-              inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <MaterialIcons name="date-range" size={24}/>
-              }
-              onChangeText={props.handleChange("dOB")}
-              value={props.values.dOB}
+            <Text style={globalStyles.inputError}>{errors.gender}</Text>
+
+            {/*Date of Birth*/}
+            <TouchableOpacity
+              style={styles.dOBContainer}
+              onPress={showDatePicker}
+            >
+              <Icon name="calendar" type="material-community" size={24} color="black" />
+              {isDateConfirmed ? (
+                <Text style={styles.dOBText}>{moment(date).format("DD/MM/YYYY")}</Text>
+              ): (
+                <Text style={styles.dOBPlaceholderText}>Date of Birth</Text>
+              )}
+            </TouchableOpacity>
+            <View 
+              style={styles.dOBLine}
             />
-            <Input 
-              containerStyle={globalStyles.inputContainerNormal}
-              placeholder="Faculty"
-              inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <FontAwesome5 name="school" size={18}/>
-              }
-              onChangeText={props.handleChange("faculty")}
-              value={props.values.faculty}
+            
+            <DateTimePickerModal 
+              isVisible={isDatePickerVisible}
+              mode="date"
+              onConfirm={(date) => {
+                hideDatePicker();
+                setDate(date);
+                setFieldValue("dOB", moment(date).format("DD/MM/YYYY"));
+                setDateConfirmed(true);
+              }}
+              onCancel={hideDatePicker}
+              date={date}
+              onChange={(event, selectedDate) => {
+                const currentDate = selectedDate || date; 
+                setDate(currentDate);
+                setFieldValue(
+                  "incidentDate",
+                  moment(selectedDate).format("DD/MM/YYYY")
+                )
+              }}
             />
+            <Text style={globalStyles.inputError}>{errors.dOB}</Text>
+
+            <RNPickerSelect 
+              onValueChange={(value) => setFieldValue("faculty", value)}
+              items={faculty}
+              placeholder={{
+                label: "Faculty", 
+                value: null,
+              }}
+              useNativeAndroidPickerStyle={false}
+              style={{
+                ...pickerSelectStyles, 
+                iconContainer:{ left: 30 }, 
+                placeholder: styles.dropdownPlaceholder,
+              }}
+              Icon={() => {
+                return <FontAwesome5 name="school" size={18}/>
+              }}
+            />
+            <Text style={globalStyles.inputError}>{errors.faculty}</Text>
+
             <Input 
               containerStyle={globalStyles.inputContainerNormal}
               placeholder="Set your password"
               inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <Ionicons name="lock-open" size={24}/>
-              }
-              onChangeText={props.handleChange("password")}
-              value={props.values.password}
+              leftIcon={<Ionicons name="lock-open" size={24}/>}
+              onChangeText={handleChange("password")}
+              secureTextEntry={true}
             />
+            <Text style={globalStyles.inputError}>{errors.password}</Text>
+
             <Input 
               containerStyle={globalStyles.inputContainerNormal}
               placeholder="Confirm your password"
               inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <Ionicons name="lock-closed" size={24}/>
-              }
-              errorMessage="Both passwords do not match. Please check again."
-              errorStyle={globalStyles.inputError}
-              onChangeText={props.handleChange("confirmPassword")}
-              value={props.values.confirmPassword}
+              leftIcon={<Ionicons name="lock-closed" size={24}/>}
+              onChangeText={handleChange("confirmPassword")}
+              secureTextEntry={true}
             />
+            <Text style={globalStyles.inputError}>{errors.confirmPassword}</Text>
 
             <TouchableOpacity
               style={globalStyles.buttonTop}
-              onPress={props.handleSubmit}
-              onPress={() => navigation.navigate("Main Tab Navigator")}
+              onPress={handleSubmit}
+              disabled={!isValid}
             >
               <Text style={globalStyles.buttonText}>Submit</Text>
             </TouchableOpacity>
           </View>
         )}
       </Formik>
-    </View>
+      </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
+
+const gender = [
+  { value: "male", label: "Male" }, 
+  { value: "female", label: "Female" }, 
+  { value: "preferNotToSay", label: "Prefer not to say"}
+]
+
+const faculty = [
+  { value: "FASS", label: "Faculty of Arts & Social Sciences"}, 
+  { value: "FoD", label: "Faculty of Dentistry" }, 
+  { value: "FoE", label: "Faculty of Engineering" }, 
+  { value: "FoL", label: "Faculty of Law" }, 
+  { value: "FoM", label: "Faculty of Medicine" }, 
+  { value: "FoS", label: "Faculty of Science" }, 
+  { value: "NBS", label: "NUS Business School" }, 
+  { value: "SoC", label: "School of Computing" },
+  { value: "SDE", label: "School of Design & Environment" }, 
+  { value: "SoPH", label: "SSH School of Public Health" }, 
+  { value: "SoPP", label: "LKY School of Public Policy" }, 
+  { value: "Yale", label: "Yale-NUS" }, 
+  { value: "YST", label: "Yong Siew Toh Conservatory of Music" }, 
+  { value: "Staff", label: "Staff" },
+  { value: "Others", label: "Others" }, 
+]
 
 const styles = StyleSheet.create({
   container: {
@@ -138,5 +262,54 @@ const styles = StyleSheet.create({
   },
   genderButton: {
     color: "#ff0000",
+  },
+  dropdownPlaceholder: {
+    color: colors.darkGrey, 
+    paddingLeft: 30,
+    fontSize: 15,
+  },
+  dOBContainer: {
+    marginTop: -5,
+    marginHorizontal: 30,
+    flexDirection: "row",
+
+  },
+  dOBPlaceholderText: {
+    marginLeft: 5,
+    color: colors.darkGrey,
+  },
+  dOBText: {
+    marginLeft: 5, 
+    color: colors.black,
+  },
+  dOBLine: {
+    height: 1.5, 
+    marginHorizontal: 30,
+    backgroundColor: "#aeb3b8",
+    marginTop: 8,
+    marginBottom: 23,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 15,
+    paddingBottom: 12,
+    borderBottomWidth: 1.5,
+    borderColor: "#aeb3b8",
+    color: colors.black,
+    paddingLeft: 30,
+    marginHorizontal: 30,
+    marginBottom: 25,
+  },
+  inputAndroid: {
+    fontSize: 15,
+    paddingBottom: 12,
+    borderBottomWidth: 1.5,
+    borderColor: "#aeb3b8",
+    color: colors.black,
+    paddingLeft: 30,
+    marginHorizontal: 30,
+    marginBottom: 25,
   },
 });
--- screens/authentication-screens/SignUpVerificationScreen.js
+++ screens/authentication-screens/SignUpVerificationScreen.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+import * as yup from "yup";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView 
+} from "react-native";
 import { Formik } from "formik";
 import { Input } from "react-native-elements"; 
 import { globalStyles } from "../../assets/globalStyles";
@@ -7,35 +17,48 @@ import { Ionicons } from "@expo/vector-icons";
 import colors from "../../assets/colors";
 
 export default function SignUpVerificationScreen({ navigation }) {
+
+  const validationSchema = yup.object().shape({
+    code: yup.string()
+      .label('code')
+      .length(6, 'Please enter a valid verification code')
+      .required('Please enter your verification code')
+  })
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView 
+      behavior="position"
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ScrollView showsVerticalScrollIndicator={false}>
       <Formik
         initialValues={{code:""}}
+        validationSchema = {validationSchema}
         onSubmit={(values) => {
-
+          navigation.navigate("Sign Up Detail Screen")
         }}
       >
-        {(props) => (
+        {({ handleChange, handleSubmit, errors, isValid}) => (
           <View>
-            <Text style={styles.text}>Please check your email for the verification code</Text>
+            <Text style={styles.text}>Enter the verification code sent to your email.</Text>
             <Input 
               containerStyle={globalStyles.inputContainerTop}
               label="Your verification code"
               labelStyle={globalStyles.inputLabel}
               placeholder="Verification code"
               inputStyle={globalStyles.inputInput}
-              leftIcon={
-                <Ionicons name="shield-checkmark" size={24}/>
-              }
-              errorMessage="Verification code is incorrect. Please check again."
-              errorStyle={globalStyles.inputError}
-              onChangeText={props.handleChange("code")}
-              value={props.values.code}
+              leftIcon={<Ionicons name="shield-checkmark" size={24}/>}
+              onChangeText={handleChange("code")}
+              secureTextEntry={true}
             />
+
+            <Text style={globalStyles.inputError}>{errors.code}</Text>
+
             <TouchableOpacity
               style={globalStyles.buttonTop}
-              onPress={props.handleSubmit}
-              onPress={() => navigation.navigate("Sign Up Detail Screen")}
+              onPress={handleSubmit}
+              disabled={!isValid}
             >
               <Text style={globalStyles.buttonText}>Submit</Text>
             </TouchableOpacity>
@@ -47,7 +70,9 @@ export default function SignUpVerificationScreen({ navigation }) {
           </View>
         )}
       </Formik>
-    </View>
+      </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
 

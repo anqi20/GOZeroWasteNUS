6747f7aaf36d245b983ff987ed2bee0d295e7984
tsh22 <sihuitoh22@gmail.com>
2021-12-07 22:15:38 +0800

    Upload borrow data into firebase

--- screens/borrow-screens/BorrowApi.js
+++ screens/borrow-screens/BorrowApi.js
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from "react";
 import firebase from "../../database/firebaseDB";
+import moment from "moment";
 
 // Set stall details
 export function setStallDetails(
@@ -24,6 +26,7 @@ export function setStallDetails(
     });
 }
 
+// Set quotas of cup and containers
 export function setQuotas(setCupQuota, setContainerQuota) {
   firebase
     .firestore()
@@ -41,4 +44,129 @@ export function setQuotas(setCupQuota, setContainerQuota) {
     .catch((error) => {
       console.log("Error getting stall details: ", error);
     });
+}
+
+// Helper function for uploadBorrowData
+function addCupDates(uid, cups) {
+  const currDate = moment().format("DD/MM/YYYY");
+  const currTime = moment().format("hh:mm a");
+  const userRef = firebase.firestore().collection("users").doc(uid);
+  userRef
+    .get()
+    .then((document) => {
+      const retrievedData = document.data().cupDate;
+      if (retrievedData == undefined) {
+        userRef.set(
+          {
+            cupDate: [
+              {
+                time: currTime,
+                numCups: cups,
+                date: currDate,
+              },
+            ],
+          },
+          { merge: true }
+        );
+      } else {
+        userRef.set(
+          {
+            cupDate: [
+              { time: currTime, numCups: cups, date: currDate },
+              ...retrievedData,
+            ],
+          },
+          { merge: true }
+        );
+      }
+      console.log("Borrow data successfully added for cup!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// Helper function for uploadBorrowData
+function addContainerDates(uid, containers) {
+  const currDate = moment().format("DD/MM/YYYY");
+  const currTime = moment().format("hh:mm a");
+  const userRef = firebase.firestore().collection("users").doc(uid);
+  userRef
+    .get()
+    .then((document) => {
+      const retrievedData = document.data().containerDate;
+      if (retrievedData == undefined) {
+        userRef.set(
+          {
+            containerDate: [
+              {
+                time: currTime,
+                numContainers: containers,
+                date: currDate,
+              },
+            ],
+          },
+          { merge: true }
+        );
+      } else {
+        userRef.set(
+          {
+            containerDate: [
+              { time: currTime, numContainers: containers, date: currDate },
+              ...retrievedData,
+            ],
+          },
+          { merge: true }
+        );
+      }
+      console.log("Borrow data successfully added for container!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// Helper function for uploadBorrowData
+function updateTotalCups(uid, numCups) {
+  const userRef = firebase.firestore().collection("users").doc(uid);
+  userRef.update({
+    numCup: firebase.firestore.FieldValue.increment(numCups),
+  });
+}
+
+// Helper function for uploadBorrowData
+function updateTotalContainers(uid, numContainers) {
+  const userRef = firebase.firestore().collection("users").doc(uid);
+  userRef.update({
+    numContainer: firebase.firestore.FieldValue.increment(numContainers),
+  });
+}
+
+// Upload date that each item is borrowed
+export function uploadBorrowData(uid, numCups, numContainers) {
+  if (numCups > 0 && numContainers > 0) {
+    // Both cups and containers are selected
+    for (let i = 0; i < numCups; i++) {
+      addCupDates(uid, numCups);
+    }
+    for (let i = 0; i < numContainers; i++) {
+      addContainerDates(uid, numContainers);
+    }
+    updateTotalCups(uid, numCups);
+    updateTotalContainers(uid, numContainers);
+  } else if (numCups > 0) {
+    // Only cups are selected
+    for (let i = 0; i < numCups; i++) {
+      addCupDates(uid, numCups);
+    }
+    updateTotalCups(uid, numCups);
+  } else if (numContainers > 0) {
+    // Only containers are selected
+    for (let i = 0; i < numContainers; i++) {
+      addContainerDates(uid, numContainers);
+    }
+    updateTotalContainers(uid, numContainers);
+  } else {
+    return null;
+  }
 }
--- screens/borrow-screens/BorrowSuccessfulScreen.js
+++ screens/borrow-screens/BorrowSuccessfulScreen.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { StyleSheet, Text, ScrollView } from "react-native";
 import SuccessBox from "../../components/SuccessBox";
 import colors from "../../assets/colors";
 import { globalStyles } from "../../assets/globalStyles";
 import FooterText from "../../components/FooterText";
+import { UserContext } from "../../assets/UserContext";
+import { uploadBorrowData } from "./BorrowApi";
 
 export default function BorrowSuccessfulScreen({ route }) {
+  const userData = useContext(UserContext);
   const { numCups, numContainers } = route.params;
+  // console.log(userData.id);
+  useEffect(() => {
+    uploadBorrowData(userData.id, numCups, numContainers);
+  }, []);
 
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>

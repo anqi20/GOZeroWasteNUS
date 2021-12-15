200663079194965db71545c850daf55896a02fc7
tsh22 <sihuitoh22@gmail.com>
2021-12-15 20:11:14 +0800

    Fix errors

--- screens/StatsScreen.js
+++ screens/StatsScreen.js
@@ -4,40 +4,8 @@ import { Icon } from "react-native-elements";
 import { MaterialIcons } from "@expo/vector-icons";
 import colors from "../assets/colors";
 import moment from "moment";
-import { TabView, TabBar } from 'react-native-tab-view';
+import { TabView, TabBar } from "react-native-tab-view";
 import Leaderboard from "../components/Leaderboard";
-
-function ReturnDatesTab({ containerDate, cupDate, coin }) {
-  return (
-  <View style={{ flex: 1, backgroundColor: colors.white }}>
-    <View style={styles.contentContainer}>
-      <Text style={[styles.redNumber, { marginRight: 25 }]}>{containerDate.length}</Text>
-      <Image source={require("../assets/AppImages/container.png")} />
-      <View style={{ marginLeft: 30 }}>
-        <Text>Return by:{"\n"}</Text>
-        <Text>({moment("23/08/21", "DD-MM-YY").format("ddd")}) 23/08/21</Text>
-        <Text>({moment("24/8/21", "DD-MM-YY").format("ddd")}) 24/08/21</Text>
-        <Text>({moment("26/8/21", "DD-MM-YY").format("ddd")}) 26/08/21</Text>
-      </View>
-    </View>
-
-    <View style={styles.contentContainer}>
-      <Text style={styles.redNumber}>{cupDate.length}</Text>
-      <Image source={require("../assets/AppImages/cup.png")} />
-      <View style={{ marginLeft: 30 }}>
-        <Text>Return by:{"\n"}</Text>
-        <Text>({moment("23/08/21", "DD-MM-YY").format("ddd")}) 23/08/21</Text>
-        <Text>({moment("24/8/21", "DD-MM-YY").format("ddd")}) 24/08/21</Text>
-        <Text>({moment("26/8/21", "DD-MM-YY").format("ddd")}) 26/08/21</Text>
-      </View>
-    </View>
-
-    <View style={styles.contentContainer}>
-      <Image source={require("../assets/AppImages/coin.png")} />
-      <Text style={styles.number}>{coin}</Text>
-    </View>
-  </View>
-);
 
 export default function StatsScreen({ route }) {
   const [index, setIndex] = useState(0);
@@ -46,17 +14,70 @@ export default function StatsScreen({ route }) {
     { key: "second", title: "Leaderboard" },
   ]);
 
-  const containerDate = route.params.containerDate
-  const cupDate = route.params.cupDate
+  const containerDate = route.params.containerDate;
+  const cupDate = route.params.cupDate;
   const coin = route.params.coin;
 
   const renderScene = ({ route }) => {
-    switch(route.key) {
-      case "first": 
-        return <ReturnDatesTab containerDate={containerDate} cupDate={cupDate} coin={coin} />
-      case "second": 
-        return <Leaderboard />
+    switch (route.key) {
+      case "first":
+        return (
+          <ReturnDatesTab
+            containerDate={containerDate}
+            cupDate={cupDate}
+            coin={coin}
+          />
+        );
+      case "second":
+        return <Leaderboard />;
     }
+  };
+
+  function ReturnDatesTab({ containerDate, cupDate, coin }) {
+    return (
+      <View style={{ flex: 1, backgroundColor: colors.white }}>
+        <View style={styles.contentContainer}>
+          <Text style={[styles.redNumber, { marginRight: 25 }]}>
+            {containerDate.length}
+          </Text>
+          <Image source={require("../assets/AppImages/container.png")} />
+          <View style={{ marginLeft: 30 }}>
+            <Text>Return by:{"\n"}</Text>
+            <Text>
+              ({moment("23/08/21", "DD-MM-YY").format("ddd")}) 23/08/21
+            </Text>
+            <Text>
+              ({moment("24/8/21", "DD-MM-YY").format("ddd")}) 24/08/21
+            </Text>
+            <Text>
+              ({moment("26/8/21", "DD-MM-YY").format("ddd")}) 26/08/21
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.contentContainer}>
+          <Text style={styles.redNumber}>{cupDate.length}</Text>
+          <Image source={require("../assets/AppImages/cup.png")} />
+          <View style={{ marginLeft: 30 }}>
+            <Text>Return by:{"\n"}</Text>
+            <Text>
+              ({moment("23/08/21", "DD-MM-YY").format("ddd")}) 23/08/21
+            </Text>
+            <Text>
+              ({moment("24/8/21", "DD-MM-YY").format("ddd")}) 24/08/21
+            </Text>
+            <Text>
+              ({moment("26/8/21", "DD-MM-YY").format("ddd")}) 26/08/21
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.contentContainer}>
+          <Image source={require("../assets/AppImages/coin.png")} />
+          <Text style={styles.number}>{coin}</Text>
+        </View>
+      </View>
+    );
   }
 
   return (
